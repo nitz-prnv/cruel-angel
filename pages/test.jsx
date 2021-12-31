9e3ea7bdfb2348f6dfd5f3f6d2fabacf0fefdfed
nitz-prnv <nitprnv@gmail.com>
2021-12-31 23:53:52 +0530

    (>_<)

---
+++ pages/test.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ControlWrapper, PlayerWrapper, PlayPause } from "../components";
+ export default function BloodPop(){
+     return(
+         <>
+         <PlayerWrapper>
+             <ControlWrapper>
+                 <PlayPause></PlayPause>
+             </ControlWrapper>
+         </PlayerWrapper>
+         </>
+     )
+ }
